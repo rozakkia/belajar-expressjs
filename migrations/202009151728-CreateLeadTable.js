814524003dcb9bc8f55048c1ba0524253d2dddda
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-21 22:07:07 +0700

    Final Commit

---
+++ migrations/202009151728-CreateLeadTable.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { query } = require("express");
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Leads', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            email: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Leads');
+    }
+};
