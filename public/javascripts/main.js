9429e8789b27f75444d628a75773b58fb9978336
rozakkia <rozakilhamaditya@students.unnes.ac.id>
2020-09-22 16:09:59 +0700

    update typescript code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,8 @@
 "use strict";
 var form = document.querySelector('#form');
 var inputName = document.querySelector('#name');
+var inputAge = document.querySelector('#age');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(inputName.value);
+    console.log(inputName.value + inputAge.value);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 
 const form = document.querySelector('#form') as HTMLFormElement;
 const inputName = document.querySelector('#name') as HTMLInputElement;
+const inputAge = document.querySelector('#age') as HTMLInputElement;
+
 
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
     console.log(
-        inputName.value
+       inputName.value + inputAge.value
     );
 })
